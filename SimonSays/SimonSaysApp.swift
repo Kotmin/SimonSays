f0fd58f461924017dd2cd32fc82387fcb01d3e60
Kotmin <70173732+Kotmin@users.noreply.github.com>
2024-02-29 08:39:09 +0100

    Initial Commit

---
+++ SimonSays/SimonSaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonSaysApp.swift
+//  SimonSays
+//
+//  Created by Paweł Jan Tłusty on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimonSaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
